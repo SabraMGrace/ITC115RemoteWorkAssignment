f10298111529f233b4094596cc960371d3ad2cfe
SabraMGrace <sabramgrace@gmail.com>
2019-12-02 15:42:43 -0800

    Update harvardLawyer.java

--- harvardLawyer.java
+++ harvardLawyer.java
@@ -1,1 +1,26 @@
+/* Sabra Grace
+ITC 115 Remote Work Assignment
+harvardLawyer.java
+November 30, 2019
+*/
 
+// extend superclass for Lawyer class.
+public class HarvardLawyer extends Lawyer {
+    public double getSalary() {
+        return super.getSalary() * 1.2;
+    }
+    
+    public int getVacationDays() {
+        return super.getVacationDays() + 3;
+    }
+    
+    public String getVacationForm() {
+        String s = "";
+        
+        for (int i = 0; i < 4; i++) {
+            s += super.getVacationForm();
+        }
+        
+        return s;
+    }
+}
