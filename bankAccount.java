0f5b0f7fbb7c6f1e86398621a85941337498d3e5
SabraMGrace <sabramgrace@gmail.com>
2019-12-02 15:40:01 -0800

    Update bankAccount.java

--- bankAccount.java
+++ bankAccount.java
@@ -1,1 +1,22 @@
+/* Sabra Grace
+ITC 115 Remote Work Assignment
+bankAccount.java
+November 30, 2019
+*/
 
+// create method for transaction fee.
+public boolean transactionFee(double fee) {
+    double deduction = 0.0;
+    
+    for (int i = 1; i <= transactions; i++) {
+        deduction += i * fee;
+    }
+    
+    if (deduction < balance) {
+        balance -= deduction;
+        return true;
+    }
+    
+    balance = 0.0;
+    return false;
+}
